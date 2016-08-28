ebb0253e77b547e5c319a6c8329b51578a420e5e
Karina Florez <florez.chokewanca@gmail.com>
2016-08-28 13:15:58 -0500

    updating files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# simple-calculator
+# Simple-calculator
+
+This files contains an attempt to build a simple calculator using html and javascript as an exercise to improve javascipt skills for beginners. 
+
+Kiri is a very lazy cat and spends a lot of money on cat treats. She can never guess how much she is spending or how much she need to buy her treats. In order to help Kiri you would need to build a simple calculator that kiri can understand and use. You need to use all the math and relational operators you have leanrt in class. 
+
+## Pseudocode 
---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+
+function addition (numero1 , numero2)
+{
+  var numero1 = numero1;
+  var numero2 = numero2;
+  var resultado= numero1 + numero2;
+  return document.write ( "the addition result is :" + resultado);
+}
+function substraction (numero1 , numero2)
+{
+  var numero1 = numero1;
+  var numero2 = numero2;
+  var resultado= numero1 - numero2;
+  return document.write ( "the substraction result is :" + resultado);
+}
+function multiplication (numero1 , numero2)
+{
+  var numero1 = numero1;
+  var numero2 = numero2;
+  var resultado= numero1 * numero2;
+  return document.write ( "the multiplication result is :" + resultado);
+}
+function division (numero1 , numero2)
+{
+  var numero1 = numero1;
+  var numero2 = numero2;
+  var resultado= numero1 / numero2;
+  return document.write ( "the division result is :" + resultado);
+}
+
+
+var menu = prompt("Enter an option: addition, substraction, multiplication or division")
+                
+switch (menu)
+  {
+    case"addition":
+    case"ADDITION":
+    case"suma":
+    var num1 = parseInt ( prompt ("enter a number")); 
+    var num2 = parseInt ( prompt ("enter a number")); 
+    addition (num1,num2 );
+    break;
+    
+    case "substraction":
+    case" SUBSTRACTION ":
+    case"resta":
+    var num1 = parseInt ( prompt ("enter a number")); 
+    var num2 = parseInt ( prompt ("enter a number")); 
+    substraction (num1,num2 );
+    break;
+      
+    case "multiplication":
+    case"MULTIPLICATION":
+    case"multiplicación":
+    var num1 = parseInt ( prompt ("enter a number")); 
+    var num2 = parseInt ( prompt ("enter a number")); 
+    multiplication (num1,num2 );
+    break;
+       
+    case "division":
+    case"DIVISION":
+    case"división":
+    var num1 = parseInt ( prompt ("enter a number")); 
+    var num2 = parseInt ( prompt ("enter a number")); 
+    division (num1,num2 );
+    break;
+  
+  }
